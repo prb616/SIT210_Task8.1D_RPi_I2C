7cf8fa1ca8ccbab79eac2b5fdf37d68424905876
prb616 <127139401+prb616@users.noreply.github.com>
2023-10-22 22:07:57 +0530

    Update SIT210_Task8.1D_RPi_I2C.py

--- SIT210_Task8.1D_RPi_I2C.py
+++ SIT210_Task8.1D_RPi_I2C.py
@@ -1,37 +1,4 @@
-import smbus
-import time
-
-
-BH1750 = 0x23 
-CONTINUOUS_HIGH_RES_MODE_1 = 0x10
-
-bus = smbus.SMBus(1)
-
-def ReadBH1750(device_address):
-  
-  data = bus.read_i2c_block_data(device_address,CONTINUOUS_HIGH_RES_MODE_1)
-  result=(data[1] + (256 * data[0])) / 1.2
-  return result
-
-
-
-try:
-	while 1:
-            LightLevel=ReadBH1750(BH1750)
-            if LightLevel < 5:
-                print ("Too Dark")
-            elif LightLevel < 10:
-                print ("Dark")
-            elif LightLevel < 20:
-                print ("Medium")
-            elif LightLevel < 25:
-                print ("Bright")
-            else:
-                print ("Too Bright")
-            time.sleep(1)
-                
-except KeyboardInterrupt:
-	GPIO.cleanup()# Import necessary libraries
+# Import necessary libraries
 import smbus
 import time
 
@@ -74,4 +41,4 @@ try:
                 
 except KeyboardInterrupt:
     # Handle keyboard interrupt by cleaning up GPIO (Note: No GPIO cleanup in the code, seems to be missing or unrelated)
-    pass
+GPIO.cleanup()
